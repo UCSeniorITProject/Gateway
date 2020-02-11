be9488fcf452a17c566bb71886e6cedcefdaa30c
Samuel Curry <samc735@gmail.com>
2020-02-11 13:39:00 -0500

    Update index.js

--- src/controllers/patient/index.js
+++ src/controllers/patient/index.js
@@ -3,7 +3,7 @@ const patientSchema = require('./schemas');
 
 module.exports = (fastify, options, next) => {
   fastify.post('/patient', {schema: patientSchema.createPatient}, patientService.createPatient);
-  fastify.patch('/patient/:id', {schema: patientSchema.updatePatient}, patientService.updatePatient);
+//   fastify.patch('/patient/:id', {schema: patientSchema.updatePatient}, patientService.updatePatient);
   fastify.delete('/patient/:id', {schema: patientSchema.deletePatient}, patientService.deletePatient);
   fastify.get('/patient', {schema: patientSchema.getPatientWithFilter}, patientService.getPatientWithFilter);
   next();
